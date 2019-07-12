33cf844af674428d6254a9e41652b6fced5949cf
lialee <46589495+hwaria@users.noreply.github.com>
2019-07-12 18:16:04 +0900

    Update ConnectDAO.java

--- ConnectDAO.java
+++ ConnectDAO.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.stereotype.Repository;
 
 import com.moneta.framework.pjcf.dao.PaxAbstractMapper;
+import com.moneta.test.domain.CmtVO;
+import com.moneta.test.domain.Pagination;
 import com.moneta.test.domain.TestVO;
 
 @Repository("com.moneta.test.DAO.ConnectDAO") // repository랑 resource랑 어떻게 다르지?
@@ -18,35 +20,78 @@ public class ConnectDAO extends PaxAbstractMapper{
 	   super.setSqlSessionFactory(sqlSession);
     }
 	
-	
+    //게시글 목록 가져오기 
 	public List<TestVO> getList(TestVO vo){ //Resource로 가져온 SqlSeissionFactory의 sqlSession에 접근
 		
 		 return getSqlSession().selectList("com.moneta.test.mapper.newMapper.selectList",vo); // vo로 어떤 mapper의 sql명령을 쓰는 건지 알려줌
 		 //mapper에서 설정한 "selectList" 사용
     }
-
+	
+	//상세보기 
 	public TestVO getView(TestVO vo){ //Resource로 가져온 SqlSeissionFactory의 sqlSession에 접근
 		
 		 return getSqlSession().selectOne("com.moneta.test.mapper.newMapper.selectView",vo.getBoardSeq()); // vo로 어떤 mapper의 sql명령을 쓰는 건지 알려줌
 		 //mapper에서 설정한 "selectList" 사용
    }
+	
+	//게시글 삭제
 	public int deleteSeq(TestVO vo) {
-		
-		
 		return getSqlSession().delete("com.moneta.test.mapper.newMapper.deleteSeq", vo.getBoardSeq());
 	}
 	
-	public int editSeq(TestVO vo) {
-		System.out.println("수정vo DAO->" + vo);
-		
+	//게시글 수정
+	public int editSeq(TestVO vo) {	
 		return getSqlSession().update("com.moneta.test.mapper.newMapper.editSeq", vo);
 	}
 	
+	//게시글 생성
 	public int createContent(TestVO vo) {
-		System.out.println("글 생성 dao ->");
-		return getSqlSession().insert("com.moneta.test.mapper.newMapper.createSeq", vo);
-			
+		return getSqlSession().insert("com.moneta.test.mapper.newMapper.createSeq", vo);	
 	}
 	
-	//게시글 목록 중 제일 마지막 board_seq 값을 받아오는 기능을 하는 메소드는 없나?
+	//총 게시글 수 받아오기
+	public int getTotalcount(){ 
+		
+		 return getSqlSession().selectOne("com.moneta.test.mapper.newMapper.countContent"); 
+   }
+	
+	//페이징 처리된 게시글 목록 가져오기
+	public List<TestVO> getTestList(Pagination pm) {
+		
+		return getSqlSession().selectList("com.moneta.test.mapper.newMapper.testlist", pm);
+	}
+	
+	//페이징 처리된 검색 목록 가져오기
+	public List<TestVO> getSearchList(Pagination pm) {
+		
+		return getSqlSession().selectList("com.moneta.test.mapper.newMapper.searchList", pm);
+	}
+	
+	//상세보기 시 조회수 증가
+	public int viewCntPlus(TestVO vo) {
+		
+		return getSqlSession().update("com.moneta.test.mapper.newMapper.viewCntPlus", vo);
+	}
+
+	//댓글 생성
+	public int uploadCmt(CmtVO vo) {
+		return getSqlSession().insert("com.moneta.test.mapper.newMapper.insertCmt", vo);
+	}
+
+	//댓글 목록 가져오기
+	public List<CmtVO> getCmtList(CmtVO vo) {
+		
+		return getSqlSession().selectList("com.moneta.test.mapper.newMapper.readCmt", vo);
+	}
+
+	//댓글 수 가져오기
+	public int getCmtTotalcount(CmtVO vo) {
+		return getSqlSession().selectOne("com.moneta.test.mapper.newMapper.cmtCount", vo); 
+	}
+	
+	//검색 결과 수 가져오기
+	public int getSearchCount(String searchWord) {
+		return getSqlSession().selectOne("com.moneta.test.mapper.newMapper.searchCount", searchWord); 
+	}
+
 }
